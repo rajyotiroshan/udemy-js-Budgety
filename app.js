337325f2aec3d780045d0e154e17e90d8bee8473
rajan <rajangreedge@gmail.com>
2019-03-18 14:29:47 +0530

    created (getInput, getDOMSTrings, DOMStrings) inside UIController

--- app.js
+++ app.js
@@ -12,7 +12,23 @@ var budgetController = (function(){
  */
 
 var UIController = (function(){
-    //Code
+    var DOMStrings =  {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+    return {
+        getInput: function(){
+            var type = document.querySelector(DOMStrings.inputType).value;//inc or exp
+            var description = document.querySelector(DOMStrings.inputDescription).value;
+            var value = document.querySelector(DOMStrings.inputValue).value;
+            return {type, description, value};
+        },
+        getDOMStrings: function(){
+            return DOMStrings;
+        }
+    }
 })();
 
 
@@ -21,18 +37,21 @@ var UIController = (function(){
  */
 
  var controller = (function(budgetCtrl, UICtrl){
+     var DOMStrings = UICtrl.getDOMStrings();
     var ctrlAddItem = function() {
     //TODO
       //1. Get the field input data.
+      var  input = UICtrl.getInput();
+      console.log(input);
+
       //2. Add the item to the budget controller.
       //3. Add the item to the UI.
       //4. Calculate the budget
       //5. Display budget on the UI.
-      console.log('Add button')
     }
 
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOMStrings.inputBtn).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event){
     if(event.key === 13 || event.which === 13) {//Enter key is presed
