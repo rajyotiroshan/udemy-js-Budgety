4ddbeea399e596a27a0ad718465b61bf001f9829
rajan <rajangreedge@gmail.com>
2019-03-20 11:09:40 +0530

    fixed UICtrl error

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 /**
+ * UI Controller
+ */
+
+var UIController = (function(){
+    var DOMStrings =  {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+    return {
+        getInput: function(){
+            var type = document.querySelector(DOMStrings.inputType).value;//inc or exp
+            var description = document.querySelector(DOMStrings.inputDescription).value;
+            var value = document.querySelector(DOMStrings.inputValue).value;
+            return {type, description, value};
+        },
+        getDOMStrings: function(){
+            return DOMStrings;
+        }
+    }
+})();
+
+
+/**
  * Global App Controller
  */
 
  var controller = (function(budgetCtrl, UICtrl){
      //
     var setupEventListener = function() {
-        var DOMStrings = UIController.getDOMStrings();
+        var DOMStrings = UICtrl.getDOMStrings();
         document.querySelector(DOMStrings.inputBtn).addEventListener('click', ctrlAddItem);
 
         document.addEventListener('keypress', function(event){
@@ -19,7 +44,7 @@
     var ctrlAddItem = function() {
     //TODO
       //1. Get the field input data.
-      var  input = UIController.getInput();
+      var  input = UICtrl.getInput();
       console.log(input);
 
       //2. Add the item to the budget controller.
@@ -43,29 +68,6 @@
 var budgetController = (function(){ 
 })();
 
-/**
- * UI Controller
- */
-
-var UIController = (function(){
-    var DOMStrings =  {
-        inputType: '.add__type',
-        inputDescription: '.add__description',
-        inputValue: '.add__value',
-        inputBtn: '.add__btn'
-    }
-    return {
-        getInput: function(){
-            var type = document.querySelector(DOMStrings.inputType).value;//inc or exp
-            var description = document.querySelector(DOMStrings.inputDescription).value;
-            var value = document.querySelector(DOMStrings.inputValue).value;
-            return {type, description, value};
-        },
-        getDOMStrings: function(){
-            return DOMStrings;
-        }
-    }
-})();
 
 /**
  * initialize application.
